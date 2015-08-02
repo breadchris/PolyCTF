0dc393bb6623059f38b6404451bf024a58718106
Christopher Thompson <chrislegolife@gmail.com>
2015-08-02 18:52:18 -0400

    Few challenges

---
+++ Introduction/Eval-uating-the-Situation/eval.py
@@ -0,0 +1,8 @@
+
+flag = "flag{this_hacking_stuff_is_fun}"
+
+while True:
+    # lol python is silly: https://docs.python.org/2/library/functions.html#input
+    what_you_entered = input("python$ ")
+    print what_you_entered
+
