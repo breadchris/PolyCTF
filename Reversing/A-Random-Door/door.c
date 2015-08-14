82066f5cb8171d1b959171776841a5d1921379a8
Christopher Thompson <chrislegolife@gmail.com>
2015-08-14 15:24:22 -0400

    Updated challenges

--- Reversing/A-Random-Door/door.c
+++ Reversing/A-Random-Door/door.c
@@ -6,12 +6,29 @@
 typedef unsigned int uint;
 
 void print_flag() {
+	char flag[64];
+	FILE* f;
+
+	f = fopen("flag", "r");
+	if (f == NULL) {
+		perror("Could not open your flag :C");
+		exit(1);
+	}
+
+	if (!fgets(flag, sizeof(flag), f)) {
+		perror("Could not read your flag :C");
+		exit(1);
+	}	
+
+	printf("Here is your flag: %s\n", flag);
 }
 
 void gen_random_nums(uint *nums, int length)
 {
+    int i;
+
     srand(time(NULL));
-    for(int i = 0; i < length; i++)
+    for(i = 0; i < length; i++)
     {
         nums[i] = rand();
     }
@@ -20,22 +37,24 @@ void gen_random_nums(uint *nums, int length)
 void validate_code(uint code, uint *nums, int length)
 {
     uint test_code = 0x1337beef;
+    int i;
 
-    for (int i = 0; i < length; i++) {
+    for (i = 0; i < length; i++) {
         test_code += nums[i] >> 7;
     }
 
-    for (int i = 0; i < length; i++) {
+    for (i = 0; i < length; i++) {
         test_code |= nums[i] >> 16;
         test_code |= (nums[i] & 0xffff) << 16;
     }
 
-    for (int i = 0; i < length; i++) {
+    for (i = 0; i < length; i++) {
         test_code ^= nums[i];
     }
 
     if (code == test_code) {
         puts("\n()()()() CODE ACCEPTED ()()()()");
+        print_flag();
     } else {
         puts("\n)(-)(-)( CODE REJECTED )(-)(-)(");
     }
@@ -45,6 +64,7 @@ void system_init()
 {
     uint rand_nums[N_RAND];
     uint code;
+    int i;
     
     gen_random_nums(rand_nums, N_RAND);
     puts("------------d--------3-------------c-");
@@ -52,24 +72,25 @@ void system_init()
     puts("------------ DOOR LOCKED -------b----");
     puts("-----1--e------------4------9--------");
     puts("--5------0------f----------------a---");
-    for (int i = 0; i < 37; i++) {
+    for (i = 0; i < 37; i++) {
         printf(".");
         fflush(stdout);
         //sleep(1);
     }
     puts("\n\n:::::: SYSTEM CORE MALFUNCTION ::::::");
-    for (int i = 1; i < N_RAND + 1; i++)
+    for (i = 1; i < N_RAND + 1; i++)
     {
         printf("------- CORE VALUE %d: %d\n", i, rand_nums[i-1]);
     }
     puts("<<<< Oh well, we will do it live <<<<");
-    for (int i = 0; i < 37; i++) {
+    for (i = 0; i < 37; i++) {
         printf(".");
         fflush(stdout);
         //sleep(1);
     }
     puts("\n\n>>>> ENTER VALIDATION CODE >>>>");
     printf("CODE: ");
+    fflush(stdout);
     scanf("%d", &code);
 
     validate_code(code, rand_nums, N_RAND);
